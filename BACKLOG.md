38fad6ffe873ca1b7878b20b522b7cac308ed886
Chris Lance <clance@freedom-platform.com>
2020-11-28 14:20:16 -0500

    Notify on update

--- BACKLOG.md
+++ BACKLOG.md
@@ -12,8 +12,13 @@
 * Notifications dialog for file I/O errors
 * Search for node (then focus, edit, etc...)
 * Arrow-key navigation
+* Tags and ontology (e.g. Function: Product Management)
+* Default fields and enter-key action for forms
 
 # Issues
 
+* *** "Save As" should update current file name (verify overall that save functionality is working...)
 * Zoom container content does not grow to remain centered
 * Schema validation for loaded files
+* Reset focus button not visible in release build
+* Can't reset selection in release build
--- src/electron/updates.js
+++ src/electron/updates.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-const { app, autoUpdater } = require('electron');
+const { app, autoUpdater, dialog } = require('electron');
 const isDev = require('electron-is-dev');
 
 if (!isDev) {
@@ -7,4 +7,18 @@ if (!isDev) {
   const feed = `${server}/update/${process.platform}/${app.getVersion()}`;
 
   autoUpdater.setFeedURL(feed);
+
+  autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+    const dialogOpts = {
+      type: 'info',
+      buttons: ['Restart', 'Later'],
+      title: 'Application Update',
+      message: process.platform === 'win32' ? releaseNotes : releaseName,
+      detail: 'A new version has been downloaded. Restart the application to apply the updates.',
+    };
+
+    dialog.showMessageBox(dialogOpts).then((returnValue) => {
+      if (returnValue.response === 0) autoUpdater.quitAndInstall();
+    });
+  });
 }
